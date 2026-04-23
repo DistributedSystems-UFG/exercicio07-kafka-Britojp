1b48e94d26520824f582add1ca41a641480a48fd
britojp <joaopedrodebritotome@discente.ufg.br>
2026-04-22 21:01:09 -0300

    Update Kafka consumer to subscribe only to TOPIC2, removing subscription to TOPIC1.

--- consumer.py
+++ consumer.py
@@ -8,6 +8,6 @@ consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 # Create consumer: Option 2 -- consume old events (uncomment to test -- and comment Option 1 above)
 #consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT], auto_offset_reset='earliest')
 
-consumer.subscribe([TOPIC1, TOPIC2])
+consumer.subscribe([TOPIC2])
 for msg in consumer:
     print (msg.value)
