facd2df27733c7323f1736e5e860d080a311cf38
britojp <joaopedrodebritotome@discente.ufg.br>
2026-04-22 21:18:18 -0300

    Refactor producer_consumer.py to generate a list of random numbers, calculate their sum, and send the result to TOPIC2 instead of processing messages from TOPIC1.

--- producer_consumer.py
+++ producer_consumer.py
@@ -2,14 +2,16 @@ from kafka import KafkaProducer
 from const import *
 import sys
 from kafka import KafkaConsumer
+import os
+import random
 
-
+def sumNumbers(numbers):
+    return sum(numbers)
 
 consumer = KafkaConsumer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 producer = KafkaProducer(bootstrap_servers=[BROKER_ADDR + ':' + BROKER_PORT])
 consumer.subscribe([TOPIC1])
-for msg in consumer:
-    print (msg.value.decode())
-    txt = "Message received: " + msg.value.decode() + " from topic " + TOPIC1
-    producer.send(TOPIC2, value=txt.encode())
-    producer.flush()
+numbers = [random.randint(1, 100) for _ in range(10)]
+txt = "The sum of the numbers is " + str(sumNumbers(numbers))
+producer.send(TOPIC2, value=txt.encode())
+producer.flush()
